536039729be97a58121a147e086b335695358562
Erick De la Cruz <erickdelacruz8599@gmail.com>
2026-02-13 15:12:21 -0600

    Primer commit chat tiempo real

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const app = express();
+
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http);
+
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+
+    console.log('Un usuario se conectó');
+
+    socket.on('chat message', (msg) => {
+
+        console.log('Mensaje recibido: ' + msg);
+
+        io.emit('chat message', msg);
+
+    });
+
+});
+
+http.listen(PORT, () => {
+
+    console.log('Servidor corriendo en puerto ' + PORT);
+
+});
